4acb9da633708a52edd0e2fab864d946355e8d71
yoosername <yoosername@local>
2017-09-18 12:14:12 +0100

    first commit

---
+++ src/main/java/com/example/AutomationApp/AutomationAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.AutomationApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomationAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/AutomationApp/AutomationAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.AutomationApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AutomationAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
